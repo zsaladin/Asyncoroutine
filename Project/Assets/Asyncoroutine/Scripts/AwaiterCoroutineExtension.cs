40abd247f279124ebdf85d5adc7086c5fa82b5d2
Daehee Kim <zsaladinz@gmail.com>
2017-09-16 04:47:00 +0900

    AwaiterCoroutineExtension :  Add extension methods related with coroutine

--- Project/Assets/Asyncoroutine/Scripts/AwaiterCoroutineExtension.cs
+++ Project/Assets/Asyncoroutine/Scripts/AwaiterCoroutineExtension.cs
@@ -23,9 +23,44 @@ namespace Asyncoroutine
             return new AwaiterCoroutine<WaitForSeconds>(waitForSeconds);
         }
 
+        public static AwaiterCoroutine<WaitForSecondsRealtime> GetAwaiter(this WaitForSecondsRealtime waitForSecondsRealtime)
+        {
+            return new AwaiterCoroutine<WaitForSecondsRealtime>(waitForSecondsRealtime);
+        }
+
+        public static AwaiterCoroutine<WaitForEndOfFrame> GetAwaiter(this WaitForEndOfFrame waitForEndOfFrame)
+        {
+            return new AwaiterCoroutine<WaitForEndOfFrame>(waitForEndOfFrame);
+        }
+
+        public static AwaiterCoroutine<WaitForFixedUpdate> GetAwaiter(this WaitForFixedUpdate waitForFixedUpdate)
+        {
+            return new AwaiterCoroutine<WaitForFixedUpdate>(waitForFixedUpdate);
+        }
+
+        public static AwaiterCoroutine<WaitUntil> GetAwaiter(this WaitUntil waitUntil)
+        {
+            return new AwaiterCoroutine<WaitUntil>(waitUntil);
+        }
+
+        public static AwaiterCoroutine<WaitWhile> GetAwaiter(this WaitWhile waitWhile)
+        {
+            return new AwaiterCoroutine<WaitWhile>(waitWhile);
+        }
+
         public static AwaiterCoroutine<WWW> GetAwaiter(this WWW www)
         {
             return new AwaiterCoroutine<WWW>(www);
+        }
+
+        public static AwaiterCoroutine<AsyncOperation> GetAwaiter(this AsyncOperation asyncOperation)
+        {
+            return new AwaiterCoroutine<AsyncOperation>(asyncOperation);
+        }
+
+        public static AwaiterCoroutine<CustomYieldInstruction> GetAwaiter(this CustomYieldInstruction customYieldInstruction)
+        {
+            return new AwaiterCoroutine<CustomYieldInstruction>(customYieldInstruction);
         }
     }
 }
