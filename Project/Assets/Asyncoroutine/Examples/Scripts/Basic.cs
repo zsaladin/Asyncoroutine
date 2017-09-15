f611698e1e357be9b3612e42487b646b448797aa
Daehee Kim <zsaladinz@gmail.com>
2017-09-16 04:07:05 +0900

    Update exampl basic

--- Project/Assets/Asyncoroutine/Examples/Scripts/Basic.cs
+++ Project/Assets/Asyncoroutine/Examples/Scripts/Basic.cs
@@ -17,7 +17,33 @@ namespace Asyncoroutine.Example
             Debug.Log("Delay");
 
             WWW www = await new WWW("http://google.com");
-            Debug.Log(www.text);     
+            Debug.Log(www.text);
+
+            await new WaitForSecondsRealtime(1f);
+            Debug.Log("WaitForSecondsRealtime");
+
+            await UnityCoroutine();
+            Debug.Log("UnityCoroutine");
+        }
+
+        private IEnumerator UnityCoroutine()
+        {
+            Debug.Log("UnityCoroutine1");
+
+            yield return new WaitForSecondsRealtime(1f);
+
+            Debug.Log("UnityCoroutine2");
+
+            if (Random.value < 0.5f)
+            {
+                yield return new WaitForSeconds(0.5f);
+                Debug.Log("UnityCoroutine3");
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.0f);
+                Debug.Log("UnityCoroutine4");
+            }
         }
     }
 }
